ebb76a758040e8a3f1e8279ff4d5cbd095c0aca4
Koray Can <92692546+Kcygt@users.noreply.github.com>
2023-11-14 10:07:30 +0000

    fixing forward kinematic bug in z axes

--- PhantomClone/DAQBoard.h
+++ PhantomClone/DAQBoard.h
@@ -19,7 +19,7 @@ class DAQBoard
 		int readEncoder(vector<unsigned int>& ENCDAT1, vector<unsigned int>& ENCDAT2);
 		int readEncoderwithtimestamp(vector<unsigned int>& ENCDAT1, vector<unsigned int>& ENCDAT2);
 		double AngleCalc(unsigned int value, float GearRatio, int check, bool base);
-		double PositionCalc(vector<unsigned int> ENCDAT, int check);		
+		double PositionCalc( vector<unsigned int> ENCDAT, int check);
 		double forwardKinematics(int check, double& x1, double& y1, double& z1, double& x2, double& y2, double& z2);
 		std::vector<std::vector<double>> Jacobian(vector<unsigned int> ENCDAT, float gearRatio[3],  int check);
 		
--- Test/main.cpp
+++ Test/main.cpp
@@ -6,21 +6,18 @@ int main()
 {
 
 	int boardNum = Board.openBoard();
-	int check;
+	int check,x;
 	cout << "For zero start enter 0 " << endl;
 	cin >> check;
 	Board.configuration(check, boardNum);
 	vector<unsigned int> a, b;
-	double aa;
-	cout << "putnumber for aa value " << endl;
-	cin >> aa;
-	
-	Board.readEncoder(a, b);
-	
-	double x1, y1, z1,x2,y2,z2;
-	for (int ii = 0; ii < 10000; ii++) {
+	std::cout << "Running the code" << std::endl;
+	// after confiduration
+	double x1, y1, z1,x2,y2,z2 = 0.0;
+	for (int ii = 0; ii < 100000; ii++) {
 		Board.forwardKinematics( check, x1, y1, z1, x2, y2, z2);
-		std::cout << "x1:" << x1 << "     x2:" << x2 << std::endl;
+		std::cout << "x1  : " << x1 << "  y1   : " << y1 << "  z1   : " << z1 << std::endl;
+		//std::cout << "x1:" << x1 << "     x2:" << x2 << std::endl;
 	
 	}
 
